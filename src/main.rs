8e73a46eb1dc2df50f95495aa91218e1e11bbc67
Gabe Venberg <gabevenberg@gmail.com>
2025-07-30 01:10:32 +0200

    Got mqtt working!

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,23 @@
 mod tmc2209;
 mod wifi;
 
-use defmt::info;
+use defmt::{error, info};
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_net::{DhcpConfig, StackResources};
+use embassy_net::dns::DnsQueryType;
+use embassy_net::tcp::TcpSocket;
+use embassy_net::{DhcpConfig, IpEndpoint, StackResources};
 use embassy_time::{Duration, Timer};
 use esp_hal::clock::CpuClock;
-use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull};
+use esp_hal::rng::Rng;
 use esp_hal::timer::systimer::SystemTimer;
 use esp_hal::timer::timg::TimerGroup;
 use esp_hal::uart::{Config, Uart};
 use esp_wifi::EspWifiController;
 use panic_rtt_target as _;
+use rust_mqtt::client::client::MqttClient;
+use rust_mqtt::client::client_config::{ClientConfig, MqttVersion};
 use static_cell::StaticCell;
 use tmc2209::Tmc2209;
 use wifi::{connection, network_task};
@@ -33,22 +38,22 @@ const HOSTNAME: &str = env!("HOSTNAME");
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let peripherals = esp_hal::init(config);
+    let mut peripherals = esp_hal::init(config);
 
     esp_alloc::heap_allocator!(size: 64 * 1024);
 
+    // gotten from https://github.com/esp-rs/esp-hal/issues/2372, use clonable trng when possible.
+    let mut rng = Rng::new(peripherals.RNG.reborrow());
     let timer0 = SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(timer0.alarm0);
 
     info!("Embassy initialized!");
 
-    let mut rng = esp_hal::rng::Rng::new(peripherals.RNG);
     let timer1 = TimerGroup::new(peripherals.TIMG0);
 
     static WIFI_INIT: StaticCell<EspWifiController> = StaticCell::new();
     let wifi_init: &'static EspWifiController = WIFI_INIT.init_with(|| {
-        esp_wifi::init(timer1.timer0, rng, peripherals.RADIO_CLK)
-            .expect("Failed to initialize WIFI/BLE controller")
+        esp_wifi::init(timer1.timer0, rng).expect("Failed to initialize WIFI/BLE controller")
     });
 
     let (wifi_controller, interfaces) = esp_wifi::wifi::new(wifi_init, peripherals.WIFI)
@@ -71,6 +76,10 @@ async fn main(spawner: Spawner) {
 
     let step_pin = Output::new(peripherals.GPIO7, Level::Low, OutputConfig::default());
     let dir_pin = Output::new(peripherals.GPIO6, Level::Low, OutputConfig::default());
+    let _enstop_pin = Input::new(
+        peripherals.GPIO10,
+        InputConfig::default().with_pull(Pull::Up),
+    );
     let uart = Uart::new(
         peripherals.UART0,
         Config::default()
@@ -100,6 +109,45 @@ async fn main(spawner: Spawner) {
         Timer::after(Duration::from_millis(500)).await;
     }
     info!("got IP: {}", stack.config_v4().unwrap().address);
+
+    const BROKER_HOST: &str = "linuxgamingrig.local";
+    // open tcp socket:
+    let mqtt_broker_address = stack
+        .dns_query(BROKER_HOST, DnsQueryType::A)
+        .await
+        .unwrap();
+    info!("broker adress is {}", mqtt_broker_address);
+    let mqtt_endpoint = IpEndpoint::new(mqtt_broker_address[0], 1883);
+    let mut rx_buffer = [0; 1024];
+    let mut tx_buffer = [0; 1024];
+    let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+    socket.connect(mqtt_endpoint).await.unwrap();
+    info!("connected to endpoint");
+
+    //mqtt connection.
+    let mut config = ClientConfig::new(MqttVersion::MQTTv5, rng);
+    config.max_packet_size = 100;
+    let mut recv_buffer = [0; 1024];
+    let recv_buffer_len = recv_buffer.len();
+    let mut write_buffer = [0; 1024];
+    let write_buffer_len = recv_buffer.len();
+    let mut client = MqttClient::<_, 5, Rng>::new(
+        socket,
+        &mut write_buffer,
+        write_buffer_len,
+        &mut recv_buffer,
+        recv_buffer_len,
+        config,
+    );
+    client.connect_to_broker().await.unwrap();
+    info!("connected to mqtt!");
+    loop {
+        Timer::after(Duration::from_secs(30)).await;
+        match client.send_ping().await {
+            Ok(_) => continue,
+            Err(e) => error!("mqtt error: {}", e),
+        };
+    }
 }
 
 #[embassy_executor::task]
